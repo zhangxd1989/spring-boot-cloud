2618768bd49e6056317e132829784ad5b561ca77
zhangxd1989 <zhangxd_2011sz@163.com>
2017-05-05 16:52:58 +0800

    add 监控中心

---
+++ monitor/src/main/java/cn/zhangxd/monitor/MonitorApplication.java
@@ -0,0 +1,44 @@
+package cn.zhangxd.monitor;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+@EnableTurbineStream
+public class MonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+
+    @Configuration
+    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // Page with login form is served as /login.html and does a POST on /login
+            http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
+            // The UI does a POST on /logout on logout
+            http.logout().logoutUrl("/logout");
+            // The ui currently doesn't support csrf
+            http.csrf().disable();
+
+            // Requests for the login page and the static assets are allowed
+            http.authorizeRequests()
+                    .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
+                    .permitAll();
+            // ... and any other request needs to be authorized
+            http.authorizeRequests().antMatchers("/**").authenticated();
+
+            // Enable so that the clients can authenticate via HTTP basic for registering
+            http.httpBasic();
+        }
+    }
+}

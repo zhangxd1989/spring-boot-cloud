d83070c601105c1f4a08f77a352f985b6ea5600d
zhangxd1989 <zhangxd_2011sz@163.com>
2017-05-08 20:36:45 +0800

    add config repo

--- svcb-service/src/main/java/cn/zhangxd/svcb/ServiceBApplication.java
+++ svcb-service/src/main/java/cn/zhangxd/svcb/ServiceBApplication.java
@@ -2,10 +2,12 @@ package cn.zhangxd.svcb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class ServiceBApplication {
 
     public static void main(String[] args) {

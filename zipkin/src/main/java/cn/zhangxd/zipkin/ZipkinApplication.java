2a4a65abe9c81ef7200ec892814594fed0156dc9
zhangxd1989 <zhangxd_2011sz@163.com>
2017-05-09 18:32:04 +0800

    add zipkin server

---
+++ zipkin/src/main/java/cn/zhangxd/zipkin/ZipkinApplication.java
@@ -0,0 +1,18 @@
+package cn.zhangxd.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+/**
+ * @author zhangxd
+ */
+@SpringBootApplication
+@EnableZipkinStreamServer
+public class ZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+
+}
